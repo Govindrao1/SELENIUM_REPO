6b52bcbaa5ef3eb92e3852e065b661584d741182
HP <HP@192.168.1.11>
2023-02-27 23:43:28 +0530

    this is my third commit

---
+++ org.github/src/test/java/pack_1/Test1.java
@@ -0,0 +1,11 @@
+package pack_1;
+
+public class Test1 {
+   public static void main(String[] args) {
+	  String st="i love india";
+	  for(int i=st.length()-1;i>=0;i--)
+	  {
+		 System.out.print(st.charAt(i)); 
+	  }
+}
+}
